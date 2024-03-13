90b9e96f6844e92138c202369cb28d24aa9f9a03
ChristopherOrtiz69 <christopher.villafanet@gmail.com>
2024-03-13 10:20:27 -0600

    cambios

--- App-BiblioTechnia/Prueba Libro/Properties/Resources.Designer.cs
+++ App-BiblioTechnia/Prueba Libro/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace Prueba_Libro.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Byte[].
         /// </summary>
+        internal static byte[] BadBunny {
+            get {
+                object obj = ResourceManager.GetObject("BadBunny", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
         internal static byte[] balnearios {
             get {
                 object obj = ResourceManager.GetObject("balnearios", resourceCulture);
